a5cc1330b882dbba28b48548e27d13b5604dbd2c
David Rakosi <david.rakosi99@gmail.com>
2020-06-13 01:09:29 +0200

    CHNG: finished up
    added ability to upload new posts and images

--- exercises/js/new-post.js
+++ exercises/js/new-post.js
@@ -7,4 +7,24 @@ const API_URL = "http://localhost:3000/api/posts";
 const submitNewPost = () => {
     // HINT: Use FormData to store data to send over
     // HINT: Redirect the user to home page after successful submission
+    const postTitle = document.getElementById('form-post-title').value
+    const postContent = document.getElementById('form-post-content').value
+    const postTSSTring = Date.now()
+    const postImageInput = document.getElementById('form-post-image')
+
+    let data = new FormData();
+    data.append('post-image', postImageInput.files[0])
+    data.append('title', postTitle)
+    data.append('content', postContent)
+
+    const submitPost = fetch(API_URL, {
+        method: 'POST',
+        body: data
+    }).then(() => {
+        alert('Success!')
+
+        setTimeout(() => {
+            window.location.href = 'index.html'
+        }, 1000)
+    })
 }
